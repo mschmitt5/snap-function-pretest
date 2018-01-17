d051aa51d978ced70bea9da73b2f03ceb5a980db
Mary <schmitt.mary7@gmail.com>
2018-01-17 08:23:58 -0700

    commit first function

---
+++ life.js
@@ -0,0 +1,10 @@
+let meaningOfLife = 12;
+let meaningOfUniverse = 30;
+
+let answerToEverything = meaningOfLife + meaningOfUniverse === 42;
+
+if (answerToEverything) {
+    console.log ("True")
+} else {
+    console.log ("False")
+}
