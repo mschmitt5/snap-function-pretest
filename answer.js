087b20f531602350de87d4a377a18a43ad71040d
Mary <schmitt.mary7@gmail.com>
2018-01-17 17:00:24 -0700

    added answer files

--- answer.js
+++ answer.js
@@ -5,3 +5,16 @@ function isAnswer(guess) {
         return(false);
     }
 }
+
+function capitalizeFirstLetter(word) {
+    let newWord = word.charAt(0).toUpperCase()+word.substring(1);
+    return(newWord);
+}
+
+function sumArray(array) {
+    sum = 0.0;
+    for(i = 0; i < count(array); i++) {
+        sum = sum + array[i];
+    }
+    return(sum);
+}
--- answer.php
+++ answer.php
@@ -14,4 +14,36 @@ function isAnswer(int $guess) : bool {
     }
 }
 
-function capitalizeFirstLetter(string $word) : string {}
+function capitalizeFirstLetter(string $word) : string {
+    $newWord = strtoupper($word[0]) . substr($word, 1);
+    return($newWord);
+}
+
+function sumArray(array $array) : float {
+    $sum = 0.0;
+    for($i = 0; $i < count($array); $i++) {
+        $sum = $sum + $array[$i];
+    }
+    return($sum);
+}
+
+function stringRepeat(string $word, int $numTimes) : string {
+    $repeated = "";
+    for ($i = 0; $i < $numTimes; $i++) {
+        $repeated = $repeated . $word;
+    }
+    return($repeated);
+}
+
+function dotProduct(array $left, array $right) : float {
+    if(count($left) === count($right)) {
+        $product = 0.0;
+        for ($i = 0; $i < count($left); $i++) {
+           $product = $product + $left[$i] * $right[$i];
+        }
+        return($product);
+    } else {
+        echo "Arrays must be the same size." . PHP_EOL;
+        return(0.0);
+    }
+}
